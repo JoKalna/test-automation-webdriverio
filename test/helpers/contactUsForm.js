0b76213507b5840278a72d9c914d99290459b1ce
Madara <madara.k@hotmail.co.uk>
2020-10-21 21:08:57 +0100

    Added in xpath and value for contact us form

---
+++ test/helpers/contactUsForm.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //fill in the message box
+  inputMessage(){
+    const inputMessage = $('//div[@class="col-xs-12 col-md-9"]//div[@class="form-group"]/textarea');
+    inputMessage.setValue('THIS IS A TEST');
+  }
+  //subject Heading
+
+  //Email Address
+
+  //Order Reference
+
+  //Click send
+
+};
--- test/specs/main.js
+++ test/specs/main.js
@@ -1,8 +1,11 @@
+//const contactUsForm = require('./contactUsForm');
+const contactUsForm = require('../../test/helpers/contactUsForm');
+
 describe('Click contact form', () => {
   it('This will click a button', () =>  {
     browser.url('http://automationpractice.com/index.php');
     const contactUsLink = $(`#contact-link`);
     contactUsLink.click()
-    browser.pause(5000);
+    contactUsForm.inputMessage();
   });
 });
