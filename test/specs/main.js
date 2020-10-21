095cd391b1cd438fa03df915cf6421f745531214
Madara <madara.k@hotmail.co.uk>
2020-10-21 20:36:13 +0100

    Changed const name to contact us and added a new file

--- test/specs/main.js
+++ test/specs/main.js
@@ -1,8 +1,8 @@
 describe('Click contact form', () => {
   it('This will click a button', () =>  {
     browser.url('http://automationpractice.com/index.php');
-    const footerButton = $(`#contact-link`);
-    footerButton.click()
+    const contactUsLink = $(`#contact-link`);
+    contactUsLink.click()
     browser.pause(5000);
   });
 });
