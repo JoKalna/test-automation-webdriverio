ff9524f44aaa6141c442cf1376512a586886e1fe
Madara <madara.k@hotmail.co.uk>
2020-10-26 18:10:23 +0000

    WIP: search and go to checkout

--- test/specs/main.js
+++ test/specs/main.js
@@ -1,8 +1,35 @@
 const contactUsForm = require("../../test/helpers/contactUsForm");
 
 describe("Click contact us and fill out the form", () => {
-  it("Fill out contact us form", () => {
-    browser.url("http://automationpractice.com/index.php");
+  it.skip("Fill out contact us form", () => {
+    browser.url("/index.php");
     contactUsForm.fillContactUsFormandVerify();
+  });
+});
+
+describe("Search for dress and add to basket", () => {
+  it("Search for dress and add to basket to verify", () => {
+    browser.url("/index.php");
+    //##### search
+    const searchTerm = $(
+      '//form[@id="searchbox"]/input[@class="search_query form-control ac_input"]'
+    );
+    searchTerm.setValue("dress");
+    //##### click-product
+    const clickSearch = $('//form[@id="searchbox"]/button');
+    clickSearch.click();
+    browser.pause(5000);
+    //verify
+    const searchTermMatch = $(
+      '//div[@id="center_column"]/h1/span[@class="lighter"]'
+    ).getText();
+    console.log("WHATS INSIDE", searchTermMatch);
+    expect(searchTermMatch).to.equal(`"DRESS"`);
+
+    //add to basket on PDP
+
+    //on the form click on 'proceed to checkout'
+
+    //click in SHOPPING-CART SUMMARY
   });
 });
