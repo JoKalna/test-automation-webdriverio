51cc76c4b42052451f35bc781fb6f6ba346b53c1
Madara <madara.k@hotmail.co.uk>
2020-11-16 22:57:37 +0000

    add verify step for the price

--- test/helpers/checkout.js
+++ test/helpers/checkout.js
@@ -1,18 +1,24 @@
 const pdp = require("./pdp");
 module.exports = {
   shoppingChartSummary(pdpPrice) {
-    this.orderSummaryTitle();
+    this.checkoutSummaryPrice();
+    //this.orderSummaryTitle();
     this.productVerify(pdpPrice);
   },
 
-  orderSummaryTitle() {
-    const productTitle = $("#cart_summary > tbody > tr > td > p > a");
-    productTitle.getText();
-    console.log("whats inside checkout", productTitle.getText());
-    return productTitle;
+  // orderSummaryTitle() {
+  //   const productTitle = $("#cart_summary > tbody > tr > td > p > a");
+  //   productTitle.getText();
+  //   console.log("whats inside checkout", productTitle.getText());
+  //   return productTitle;
+  // },
+  checkoutSummaryPrice() {
+    browser.pause(5000);
+    const checkoutPrice = $("#total_product");
+    const price = checkoutPrice.getText();
+    return price;
   },
   productVerify(pdpPrice) {
-    expect(pdpPrice).to.equal();
+    expect(pdpPrice).to.equal(this.checkoutSummaryPrice());
   },
-  //checkout price title
 };
--- test/helpers/pdp.js
+++ test/helpers/pdp.js
@@ -2,27 +2,17 @@ const productGrid = require("./productGrid");
 module.exports = {
   pdp() {
     const pdpPrice = this.getPrice();
-    this.enterQuantity();
     this.addToBasket();
-    browser.pause(2000);
     this.pdpTitle();
     this.proceedToCheckout();
-    browser.pause(2000);
     return pdpPrice;
   },
-
-  enterQuantity() {
-    const quantityField = $('[id="quantity_wanted"]');
-    quantityField.setValue("2");
-  },
   getPrice() {
-    //  const getPrice = $('#our_price_display').getText();
-    browser.pause(5000);
+    browser.pause(3000);
     const priceElement = $(
       '//div[@id="center_column"]//div[@class="primary_block row"]//form[@id="buy_block"]//div[@class="box-info-product"]//div[@class="price"]//span[@id="our_price_display"]'
     );
     const price = priceElement.getText();
-    console.log("whats inside pdp.js", price);
     return price;
   },
   addToBasket() {
